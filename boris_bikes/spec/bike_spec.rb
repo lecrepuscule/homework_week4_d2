ff04701577823088196e840c9286d70ba69841f3
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-01 16:25:37 +0100

    first commit

---
+++ boris_bikes/spec/bike_spec.rb
@@ -0,0 +1,20 @@
+require_relative "spec_helper"
+
+describe Bike do
+  let(:bike) {Bike.new}
+
+  it "should not be broken after we create it" do
+    expect(bike.broken?).to be false 
+  end
+
+  it "should be able to break" do
+    bike.break
+    expect(bike.broken?).to be true
+  end
+
+  it "can be fixed" do
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be false
+  end
+end
